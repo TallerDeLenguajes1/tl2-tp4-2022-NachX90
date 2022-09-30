0385ca3f90cf0f4a3f76c5b51743f6fe14b5d0b6
NachX90 <nachx90@protonmail.com>
2022-09-30 20:41:37 -0300

    Clase Teórica :D

---
+++ ClaseTeorica/MVCTest/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Models;
+namespace MVCTest.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //public string ConsultaCliente(int id, string name)
+        //{
+        //    return $"Cliente [{id}] - {name}";
+        //}
+        public IActionResult ConsultaCliente()
+        {
+            var miCliente = new Cliente();
+            miCliente.Name = "Minombre";
+            miCliente.Edad = 15;
+            return View(miCliente);
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AltaCliente(Cliente cliente)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClaseTeorica/MVCTest/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTest.Models
+{
+    public class Cliente
+    {
+        private string? name;
+        private int edad;
+
+        public string? Name { get => name; set => name = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
